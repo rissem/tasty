132f7944b34814b419bdb7db9dc62eaadbc4ae23
rissem <rissem@gmail.com>
2014-05-09 16:27:44 -0700

    add static middleware and basic html/css/js files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var serveStatic = require('serve-static');
+
+app.use(serveStatic("public"));
 
 app.get('/hello.txt', function(req, res){
   res.send('Hello World');
