77c2e3527676994d95b203dbc30f194a419d2624
rissem <rissem@gmail.com>
2014-05-09 22:14:37 -0700

    support adding bookmarks

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
 
 var Bookmark = require('./bookmark').Bookmark;
 
 app.use(serveStatic("public"));
+app.use(bodyParser());
 
 app.get('/hello.txt', function(req, res){
   res.send('Hello World');
 });
+
+app.get("/bookmarks.json", function(req, res){
+	//fetch all the bookmarks
+});
+
+app.post('/bookmark', function(req, res){
+	var bookmark = new Bookmark();
+	bookmark.title = req.body.title;
+	bookmark.description = req.body.description;
+	bookmark.save(function(err){
+		if (err){
+			console.error("ERROR", err);
+			res.send(500, "ERROR");
+		}
+		else {
+			console.log("successfully saved bookmark", bookmark);
+			res.redirect("/");
+		}
+	});
+})
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
