492249bead5bfbb98e1ddd1b9f4e42b4e40fc115
rissem <rissem@gmail.com>
2014-05-09 23:20:14 -0700

    render bookmarks on home page

--- public/tasty.js
+++ public/tasty.js
@@ -1,1 +1,11 @@
-console.log("hello client side console");
+jQuery(document).ready(function(){
+	jQuery.get("/bookmarks.json", function(data){
+		for (var i=0; i < data.length; i++){
+			var bookmark = data[i];
+			console.log("BOOKMARK", bookmark);
+			var bookmarkHtml = '<a href="' + bookmark.url + '">' + bookmark.title + "</a>";
+			bookmarkHtml += "<br /><p>" + bookmark.description + "</p>";
+			$("#bookmarks").append(bookmarkHtml);
+		}
+	});
+});
