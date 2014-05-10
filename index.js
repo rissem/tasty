cd81263d386acf3767b2e8fbcbbe2fabdb747610
rissem <rissem@gmail.com>
2014-05-10 00:19:44 -0700

    allow port to be overridden

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.post('/bookmark', function(req, res){
 	});
 })
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
